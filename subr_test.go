399634a1ece347c5272ae566b659d5549a4bf071
Mel Boyce <weird@syngin.net>
2018-10-06 00:13:17 +1000

    increase coverage

--- subr_test.go
+++ subr_test.go
@@ -2,15 +2,18 @@ package subr
 
 import (
 	"flag"
+	"fmt"
+	"strings"
 	"testing"
 )
 
 // TestSubWithFlags ...
 func TestSubWithFlags(t *testing.T) {
-	scFset := flag.NewFlagSet("subcmd", flag.ExitOnError)
+	helpString := "%s subcmd help document"
+	scFset := flag.NewFlagSet("subcmd", flag.ContinueOnError)
 	c := Cmd{
 		Name:  "subcmd",
-		Usage: "subcmd help document",
+		Usage: helpString,
 		Fset:  scFset,
 		Flags: map[string]*string{
 			"string":  scFset.String("string", "", "test string flag"),
@@ -20,11 +23,45 @@ func TestSubWithFlags(t *testing.T) {
 			"bool":  scFset.Bool("bool", false, "test bool flag"),
 			"dbool": scFset.Bool("dbool", true, "test bool flag, default"),
 		},
+		Flagi: map[string]*int{
+			"int": scFset.Int("int", 3, "test int flag"),
+		},
 		Safeword: "halp",
 	}
 
+	eName := []string{"subcmd"}
+	c.Name = ""
+	cmd, err := Parse(eName, &c)
+	if cmd != nil {
+		t.Errorf("expected nil cmd, got %+v\n", cmd)
+	}
+	switch err := err.(type) {
+	case nil:
+		t.Error("expected ParseFailed error, got nil")
+	case *ParseFailed:
+		if fmt.Sprint(err) != "missing command name" {
+			t.Error("bad error message for missing name")
+		}
+	default:
+		t.Errorf("expected ParseFailed error, got %+v\n", err)
+	}
+	c.Name = "subcmd"
+
+	eParse := []string{"subcmd", "-b", "foo", "bar", "-a"}
+	cmd, err = Parse(eParse, &c)
+	if cmd != nil {
+		t.Errorf("expected nil cmd, got %+v\n", cmd)
+	}
+	switch err := err.(type) {
+	case nil:
+		t.Error("expected ParseFailed error, got nil")
+	case *ParseFailed:
+	default:
+		t.Errorf("expected ParseFailed error, got %+v\n", err)
+	}
+
 	enoArgs := []string{}
-	cmd, err := Parse(enoArgs, &c)
+	cmd, err = Parse(enoArgs, &c)
 	if cmd != nil {
 		t.Errorf("expected nil cmd, got %+v\n", cmd)
 	}
@@ -32,6 +69,9 @@ func TestSubWithFlags(t *testing.T) {
 	case nil:
 		t.Error("expected NoArgs error, got nil")
 	case *NoArgs:
+		if fmt.Sprint(err) != "no args to parse" {
+			t.Error("bad error message for no args")
+		}
 	default:
 		t.Errorf("expected NoArgs error, got %+v\n", err)
 	}
@@ -45,6 +85,9 @@ func TestSubWithFlags(t *testing.T) {
 	case nil:
 		t.Error("expected Safeword error, got nil")
 	case *Safeword:
+		if fmt.Sprint(err) != "safeword invoked" {
+			t.Error("bad error message for safeword")
+		}
 	default:
 		t.Errorf("expected Safeword error, got %+v\n", err)
 	}
@@ -58,6 +101,9 @@ func TestSubWithFlags(t *testing.T) {
 	case nil:
 		t.Error("expected UnknownSub error, got nil")
 	case *UnknownSub:
+		if fmt.Sprint(err) != "wat" {
+			t.Error("bad error message for unknown sub")
+		}
 	default:
 		t.Errorf("expected UnknownSub error, got %+v\n", err)
 	}
@@ -87,5 +133,8 @@ func TestSubWithFlags(t *testing.T) {
 	}
 	if !*cmd.Flagb["dbool"] {
 		t.Error("expected -dbool true, got false")
+	}
+	if strings.HasSuffix(fmt.Sprint(cmd), helpString) {
+		t.Error("expected print(cmd) to be correct, is not")
 	}
 }
